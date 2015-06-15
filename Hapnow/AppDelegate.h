54303ffda051e9418102e6dc774df82c387e3663
Gurpreet Singh <gurpreetsingh@Gurpreets-MacBook-Pro.local>
2015-06-15 12:05:32 +0530

    Initial Commit

---
+++ Hapnow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hapnow
+//
+//  Created by Gurpreet Singh on 6/15/15.
+//  Copyright (c) 2015 Ommzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hapnow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hapnow
+//
+//  Created by Gurpreet Singh on 6/15/15.
+//  Copyright (c) 2015 Ommzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
